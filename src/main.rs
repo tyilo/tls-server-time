52567e5bb587b9bbb5e0d57e9267dea7bb06ac4a
Asger Hautop Drewsen <asger@tyilo.com>
2023-08-20 23:00:45 +0200

    Add support for connecting to IP addresses

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
     io::{Read, Write},
-    net::TcpStream,
+    net::{SocketAddr, TcpStream},
 };
 
 use clap::Parser;
 use rustls::{
+    client::InvalidDnsNameError,
     internal::msgs::{
         alert::AlertMessagePayload,
         base::Payload,
@@ -16,14 +17,19 @@ use rustls::{
         },
         message::{MessageError, MessagePayload, OpaqueMessage},
     },
-    ContentType, HandshakeType, NamedGroup, ProtocolVersion, SignatureScheme,
+    ContentType, HandshakeType, NamedGroup, ProtocolVersion, ServerName, SignatureScheme,
     DEFAULT_CIPHER_SUITES,
 };
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
+fn parse_server_name(s: &str) -> Result<ServerName, InvalidDnsNameError> {
+    ServerName::try_from(s)
+}
+
 #[derive(Parser)]
 struct Args {
-    hostname: String,
+    #[arg(value_parser = parse_server_name)]
+    hostname: ServerName,
 
     #[arg(short, long, default_value = "443")]
     port: u16,
@@ -31,12 +37,11 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
     let client_hello = create_client_hello(&args.hostname);
 
     eprintln!("Sending client hello: {:02x?}", client_hello);
 
-    let tcp_stream = TcpStream::connect((args.hostname, args.port)).unwrap();
+    let tcp_stream = tcp_connect(&args.hostname, args.port);
     let server_hello = retrieve_server_hello(tcp_stream, &client_hello);
     let server_hello = parse_server_hello(&server_hello);
 
@@ -48,8 +53,60 @@ fn main() {
     println!("{}", date_time.format(&Rfc3339).unwrap());
 }
 
-fn create_client_hello(hostname: &str) -> Vec<u8> {
-    let dns_name_ref = hostname.try_into().unwrap();
+fn tcp_connect(server_name: &ServerName, port: u16) -> TcpStream {
+    let stream = match server_name {
+        ServerName::DnsName(dns_name) => TcpStream::connect((dns_name.as_ref(), port)),
+        ServerName::IpAddress(ip_address) => TcpStream::connect(SocketAddr::new(*ip_address, port)),
+        _ => panic!("unknown ServerName variant encountered"),
+    };
+    stream.unwrap()
+}
+
+fn create_client_hello(server_name: &ServerName) -> Vec<u8> {
+    let mut extensions = vec![
+        ClientExtension::ECPointFormats(vec![
+            ECPointFormat::Uncompressed,
+            ECPointFormat::ANSIX962CompressedPrime,
+            ECPointFormat::ANSIX962CompressedChar2,
+        ]),
+        ClientExtension::NamedGroups(vec![
+            NamedGroup::secp256r1,
+            NamedGroup::secp384r1,
+            NamedGroup::secp521r1,
+            NamedGroup::X25519,
+            NamedGroup::X448,
+            NamedGroup::FFDHE2048,
+            NamedGroup::FFDHE3072,
+            NamedGroup::FFDHE4096,
+            NamedGroup::FFDHE6144,
+            NamedGroup::FFDHE8192,
+        ]),
+        ClientExtension::SessionTicket(ClientSessionTicket::Request),
+        ClientExtension::ExtendedMasterSecretRequest,
+        ClientExtension::SignatureAlgorithms(vec![
+            SignatureScheme::RSA_PKCS1_SHA1,
+            SignatureScheme::ECDSA_SHA1_Legacy,
+            SignatureScheme::RSA_PKCS1_SHA256,
+            SignatureScheme::ECDSA_NISTP256_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384,
+            SignatureScheme::ECDSA_NISTP384_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512,
+            SignatureScheme::ECDSA_NISTP521_SHA512,
+            SignatureScheme::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512,
+            SignatureScheme::ED25519,
+            SignatureScheme::ED448,
+        ]),
+    ];
+
+    match server_name {
+        ServerName::DnsName(dns_name) => {
+            extensions.push(ClientExtension::make_sni(dns_name.borrow()));
+        }
+        ServerName::IpAddress(_) => {}
+        _ => panic!("unknown ServerName variant encountered"),
+    };
 
     let client_hello_payload = ClientHelloPayload {
         client_version: ProtocolVersion::TLSv1_2,
@@ -57,43 +114,7 @@ fn create_client_hello(hostname: &str) -> Vec<u8> {
         session_id: SessionId::random().unwrap(),
         cipher_suites: DEFAULT_CIPHER_SUITES.iter().map(|s| s.suite()).collect(),
         compression_methods: vec![Compression::Null],
-        extensions: vec![
-            ClientExtension::make_sni(dns_name_ref),
-            ClientExtension::ECPointFormats(vec![
-                ECPointFormat::Uncompressed,
-                ECPointFormat::ANSIX962CompressedPrime,
-                ECPointFormat::ANSIX962CompressedChar2,
-            ]),
-            ClientExtension::NamedGroups(vec![
-                NamedGroup::secp256r1,
-                NamedGroup::secp384r1,
-                NamedGroup::secp521r1,
-                NamedGroup::X25519,
-                NamedGroup::X448,
-                NamedGroup::FFDHE2048,
-                NamedGroup::FFDHE3072,
-                NamedGroup::FFDHE4096,
-                NamedGroup::FFDHE6144,
-                NamedGroup::FFDHE8192,
-            ]),
-            ClientExtension::SessionTicket(ClientSessionTicket::Request),
-            ClientExtension::ExtendedMasterSecretRequest,
-            ClientExtension::SignatureAlgorithms(vec![
-                SignatureScheme::RSA_PKCS1_SHA1,
-                SignatureScheme::ECDSA_SHA1_Legacy,
-                SignatureScheme::RSA_PKCS1_SHA256,
-                SignatureScheme::ECDSA_NISTP256_SHA256,
-                SignatureScheme::RSA_PKCS1_SHA384,
-                SignatureScheme::ECDSA_NISTP384_SHA384,
-                SignatureScheme::RSA_PKCS1_SHA512,
-                SignatureScheme::ECDSA_NISTP521_SHA512,
-                SignatureScheme::RSA_PSS_SHA256,
-                SignatureScheme::RSA_PSS_SHA384,
-                SignatureScheme::RSA_PSS_SHA512,
-                SignatureScheme::ED25519,
-                SignatureScheme::ED448,
-            ]),
-        ],
+        extensions,
     };
 
     let handshake_message_payload = HandshakeMessagePayload {
